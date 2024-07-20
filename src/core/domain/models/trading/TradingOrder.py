4a10d7940b8473586e0c3f48cb96ae6b4e23a3e1
brunofraga <bruno.fraga001@google.com>
2024-07-20 17:56:08 -0300

    quick fix

--- src/core/domain/models/trading/TradingOrder.py
+++ src/core/domain/models/trading/TradingOrder.py
@@ -1,9 +1,10 @@
 from typing import List
+import core.domain.models.asset.PortfolioAnalytics as p
 
 # Just to clarify
 class TradingOrder (dict):
-    def __init__(self, asset_names :  List[str]):
-        for asset_name in asset_names:
+    def __init__(self, portfolio :  p.Portfolio):
+        for asset_name in portfolio.asset_names:
             self.add_trade(asset_name, 0)
 
     def add_trade(self, asset_name : str, trade_exposure : float):
