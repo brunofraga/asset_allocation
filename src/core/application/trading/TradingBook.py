e9e1af799cfaf5c6e9f2275cc18e0ed4b3a9bb77
brunofraga <bruno.fraga001@google.com>
2024-08-01 14:08:03 -0300

    encapsulando método de achar peso com base na vol-targeting

--- src/core/application/trading/TradingBook.py
+++ src/core/application/trading/TradingBook.py
@@ -37,6 +37,8 @@ class TradingBook:
             self.strategies[strategy.strategy_name] = strategy
             self.strategy_weights[strategy.strategy_name] = 1/self.strategy_numb
 
+        self.__set_fixed_weights()
+
     
     # For a manual approach
     def set_strategy_weights(self, strategy_weights):
@@ -76,6 +78,60 @@ class TradingBook:
 
         return self.trading_results
     
+    def __calculate_weights_with_volt_targeting(self, target_date):
+        # is ok to do it?
+        if (self.days_running_all_strategies > self.days_range_to_calc_vol):
+            is_resizing_day = self.days_since_resizing >= self.days_to_realocate
+            if (is_resizing_day):
+                # pivoting:
+                pivot = self.trading_results.results.pivot_table(index=tr.DATE, columns=tr.STRATEGY, values=tr.PNL_DAILY,  aggfunc=np.sum)
+                pivot = pivot.dropna()
+
+                self.days_since_resizing = 0
+                asset_list = []
+                pivot[ga.GeneralAsset.DATE] = pivot.index
+                for strategy_name in self.strategies:
+                    self.strategy_weights[strategy_name] = 0
+                    #unpivoting:
+                    if not(self.strategies[strategy_name].is_stopped):
+                        asset_df =  pivot[[strategy_name]].unstack().reset_index(name=strategy_name)[[ga.GeneralAsset.DATE, strategy_name]].tail(self.days_range_to_calc_vol)
+                        asset_df[ga.GeneralAsset.LOG_RETURN] = asset_df[strategy_name] - asset_df[strategy_name].mean() # pnl pode ser negativo, e ai nao podemos calcular o log retorno dele
+                        gen_asset = ga.GeneralAsset(strategy_name,  asset_df)
+                        asset_list.append(gen_asset)
+
+                if (len(asset_list) == 0 ):
+                    self.book_vol = 0
+                else:
+                    strat_portfolio = p.Portfolio("strategies", asset_list)
+                    running_strategy_weights = strat_portfolio.get_vol_targeting_weights(target_date, self.days_range_to_calc_vol, self.target_vol, self.max_leverage)
+                    for strategy_name, value in running_strategy_weights.items():
+                        self.strategy_weights[strategy_name] = value
+
+
+
+                    self.book_vol = strat_portfolio.calc_portfolio_volatility_with_weights(target_date, self.days_range_to_calc_vol, running_strategy_weights)
+
+
+            else:
+                self.days_since_resizing +=1
+
+
+    def __set_fixed_weights(self):
+        self.fixed_strategy_weights = {}
+        for strategy_name in self.strategies:
+            desired_weight = 0
+
+            if ("Equity" in strategy_name):
+                desired_weight = 0.4
+            elif ("Credit" in strategy_name):
+                desired_weight = 0.3
+            elif ("Comdty" in strategy_name):
+                desired_weight = 0.2
+            elif ("FX" in strategy_name):
+                desired_weight = 0.1
+
+            self.fixed_strategy_weights[strategy_name] = desired_weight
+
     def __update_strategy_weights_by_vol_targeting(self, target_date):
         if (not(self.all_strategies_ready)):
             self.numb_strats_ready = 0
@@ -91,32 +147,10 @@ class TradingBook:
                 
         
         else:
-            # is ok to do it?
-            if (self.days_running_all_strategies > self.days_range_to_calc_vol):
-                is_resizing_day = self.days_since_resizing >= self.days_to_realocate
-                if (is_resizing_day):
-                    # pivoting:
-                    pivot = self.trading_results.results.pivot_table(index=tr.DATE, columns=tr.STRATEGY, values=tr.PNL_DAILY,  aggfunc=np.sum)
-                    pivot = pivot.dropna()
-
-                    self.days_since_resizing = 0
-                    asset_list = []
-                    pivot[ga.GeneralAsset.DATE] = pivot.index
-                    for strategy_name in self.strategies:
-                        #unpivoting:
-                        asset_df =  pivot[[strategy_name]].unstack().reset_index(name=strategy_name)[[ga.GeneralAsset.DATE, strategy_name]].tail(self.days_range_to_calc_vol)
-                        asset_df[ga.GeneralAsset.LOG_RETURN] = asset_df[strategy_name] - asset_df[strategy_name].mean() # pnl pode ser negativo, e ai nao podemos calcular o log retorno dele
-                        gen_asset = ga.GeneralAsset(strategy_name,  asset_df)
-                        asset_list.append(gen_asset)
-
-                    strat_portfolio = p.Portfolio("strategies", asset_list)
-                    self.strategy_weights = strat_portfolio.get_vol_targeting_weights(target_date, self.days_range_to_calc_vol, self.target_vol, self.max_leverage)
-
-                    self.book_vol = strat_portfolio.calc_portfolio_volatility_with_weights(target_date, self.days_range_to_calc_vol, self.strategy_weights)
-
-
-                else:
-                    self.days_since_resizing +=1
+            self.__calculate_weights_with_volt_targeting(target_date)
+            
+            # if (len(self.strategies) == 4):
+            #     self.strategy_weights = self.fixed_strategy_weights
 
 
             
